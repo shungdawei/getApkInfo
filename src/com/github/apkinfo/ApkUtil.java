e279d598acc1bc9d2924c4b1d99888b8b58fd302
git <shungdawei@foxmail.com>
2018-02-09 10:04:35 +0800

    增加.apk后缀过滤，Test代码调整

--- src/com/github/apkinfo/ApkUtil.java
+++ src/com/github/apkinfo/ApkUtil.java
@@ -20,61 +20,47 @@ public class ApkUtil {
 
 	public static ApkInfo getApkInfo(File apkFile) {
 
-		if (!apkFile.exists()) {
-			System.out.println("file " + apkFile.getPath() + " not exists!!!");
+		try {
+			
+			if (!apkFile.getName().endsWith(".apk")) {
+				System.out.println("file name [" + apkFile.getPath() + "] not end with .apk!");
+				return null;
+			}
+			
+			if (!apkFile.exists()) {
+				System.out.println("file " + apkFile.getPath() + " not exists!!!");
+				return null;
+			}
+
+			ApkInfo apkInfo = new ApkInfo();
+			SAXBuilder builder = new SAXBuilder();
+			String apkPath = apkFile.getAbsolutePath();
+			Document document = null;
+
+			apkInfo.setApkSize(apkFile.length());
+
+			document = builder.build(getXmlInputStream(apkPath));
+
+			Element root = document.getRootElement();// 跟节点-->manifest
+			apkInfo.setVersionCode(root.getAttributeValue("versionCode", NS));
+			apkInfo.setVersionName(root.getAttributeValue("versionName", NS));
+			apkInfo.setApkPackage(root.getAttributeValue("package", ""));// package不是以android:开头
+			Element elemUseSdk = root.getChild("uses-sdk");// 子节点-->uses-sdk
+			apkInfo.setMinSdkVersion(elemUseSdk.getAttributeValue("minSdkVersion", NS));
+			List<?> listPermission = root.getChildren("uses-permission");// 子节点是个集合
+			List<String> permissions = new ArrayList<>();
+			for (Object object : listPermission) {
+				String permission = ((Element) object).getAttributeValue("name", NS);
+				permissions.add(permission);
+			}
+			apkInfo.setUsesPermission(permissions);
+
+			return apkInfo;
+		} catch (Exception e) {
+			System.out.println("parse apk error.");
+			e.printStackTrace();
 			return null;
 		}
-
-		ApkInfo apkInfo = new ApkInfo();
-		SAXBuilder builder = new SAXBuilder();
-		String apkPath = apkFile.getAbsolutePath();
-		Document document = null;
-		
-		apkInfo.setApkSize(apkFile.length());
-		
-		try {
-			document = builder.build(getXmlInputStream(apkPath));
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		Element root = document.getRootElement();// 跟节点-->manifest
-		apkInfo.setVersionCode(root.getAttributeValue("versionCode", NS));
-		apkInfo.setVersionName(root.getAttributeValue("versionName", NS));
-		apkInfo.setApkPackage(root.getAttributeValue("package", ""));// package不是以android:开头
-		Element elemUseSdk = root.getChild("uses-sdk");// 子节点-->uses-sdk
-		apkInfo.setMinSdkVersion(elemUseSdk.getAttributeValue("minSdkVersion", NS));
-		List<?> listPermission = root.getChildren("uses-permission");// 子节点是个集合
-		List<String> permissions = new ArrayList<>();
-		for (Object object : listPermission) {
-			String permission = ((Element) object).getAttributeValue("name", NS);
-			permissions.add(permission);
-		}
-		apkInfo.setUsesPermission(permissions);
-		//
-		// String s = root.getAttributes().toString();
-		// String c[] = s.split(",");
-		// String versionCode = null;
-		// String versionName = null;
-		// String packageName = null;
-		// for (String a : c) {
-		// if (a.contains("versionCode")) {
-		// versionCode = a.substring(a.indexOf("versionCode=\"") + 13,
-		// a.lastIndexOf("\""));
-		// }
-		// if (a.contains("versionName")) {
-		// versionName = a.substring(a.indexOf("versionName=\"") + 13,
-		// a.lastIndexOf("\""));
-		// }
-		// if (a.contains("package")) {
-		// packageName = a.substring(a.indexOf("package=\"") + 9, a.lastIndexOf("\""));
-		// }
-		// }
-		// System.out.println("\n版本号:" + versionCode + "\n版本名:" + versionName + "\n包名:"
-		// + packageName);
-		// String str = "\n版本号:" + versionCode + "\n版本名:" + versionName + "\n包名:" +
-		// packageName;
-		// return root.getAttributes().toString();
-		return apkInfo;
 	}
 
 	private static InputStream getXmlInputStream(String apkPath) {
--- src/com/github/apkinfo/test/ConsoleTest.java
+++ src/com/github/apkinfo/test/ConsoleTest.java
@@ -8,14 +8,18 @@ import com.github.apkinfo.ApkUtil;
 public class ConsoleTest {
 	public static void main(String[] args) {
 
-		String filePath = "D:\\Users\\Desktop\\com.yqwireless1.apk";
+		String filePath = "D:\\Users\\Desktop\\apkutil.jar";// com.yqwireless1.apk
 		File apkFile = new File(filePath);
-		if (apkFile.exists()) {
-			ApkInfo apkInfo = ApkUtil.getApkInfo(apkFile);
+		// 指定文件不存在、不是.apk结尾、解析失败时返回 null
+		ApkInfo apkInfo = ApkUtil.getApkInfo(apkFile);
+		if (apkInfo != null) {
 			apkInfo.setApkName(apkFile.getName());
 			String apkInfoStr = apkInfo.toString();
 
 			System.out.println(apkInfoStr);
+		} else {
+			// 调试观察
+			System.out.println("apkInfo is null");
 		}
 	}
 }
