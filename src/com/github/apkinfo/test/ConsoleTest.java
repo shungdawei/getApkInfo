8bf30f08fff86945aad3f5e02183c931e20b86c7
git <shungdawei@foxmail.com>
2018-07-20 09:43:06 +0800

    修复路径参数截取bug；添加bin运行包

--- src/com/github/apkinfo/test/ConsoleTest.java
+++ src/com/github/apkinfo/test/ConsoleTest.java
@@ -8,12 +8,13 @@ import com.github.apkinfo.ApkUtil;
 public class ConsoleTest {
 	public static void main(String[] args) {
 
+		String fileParam = "-file=";
 		String filePath = null;
 		for (int i = 0; i < args.length; i++) {
 			// System.out.println("args[" + i + "]=" + args[i]);
-			if ((args[i]).startsWith("-file=")) {
-				filePath = args[i].substring("file=".length());
-				// System.out.println("filePath=" + filePath);
+			if ((args[i]).startsWith(fileParam)) {
+				filePath = args[i].substring(fileParam.length());
+				System.out.println("filePath=" + filePath);
 			}
 		}
 
@@ -22,13 +23,9 @@ public class ConsoleTest {
 			return;
 		}
 
-		// String filePath =
-		// "E:\\codesvn\\Pos\\branch\\4.x\\SpmsPos\\app\\NewSpmsPos_standard_v4.0.2.apk";
-		// String filePath =
-		// "D:\\Users\\Desktop\\NewSpmsPos3_v3.1.1.15_WH_parkLot_Liandi_sign.uns";//
-		// com.yqwireless1.apk
+		// String filePath = "D:\\Users\\Desktop\\com.yqwireless1.apk
 		File apkFile = new File(filePath);
-		// 指定文件不存在、不是.apk结尾、解析失败时返回 null
+		// 指定文件不存在或解析失败时返回 null
 		ApkInfo apkInfo = ApkUtil.getApkInfo(apkFile);
 		if (apkInfo != null) {
 			apkInfo.setApkName(apkFile.getName());
